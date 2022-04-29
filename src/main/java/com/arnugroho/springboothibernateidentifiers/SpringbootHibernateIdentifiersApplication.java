9566ea4d5c62e32f6d4123c22941856572bf756e
arnugroho <nugroho.akbar.r@gmail.com>
2022-04-29 08:20:26 +0700

    init project

---
+++ src/main/java/com/arnugroho/springboothibernateidentifiers/SpringbootHibernateIdentifiersApplication.java
@@ -0,0 +1,13 @@
+package com.arnugroho.springboothibernateidentifiers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootHibernateIdentifiersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootHibernateIdentifiersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/arnugroho/springboothibernateidentifiers/SpringbootHibernateIdentifiersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arnugroho.springboothibernateidentifiers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootHibernateIdentifiersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
